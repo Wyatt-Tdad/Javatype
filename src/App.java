712fc2c6aec797729dab2863ad08a7bd77983842
Wyatt Trinidad <wyatt.tdad@gmail.com>
2023-03-21 22:48:19 -0500

    push

---
+++ src/App.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String  name     ;
+        String  position ;
+        int     salary   ;
+        Date    hiredDate;
+    }
+}
+System.out.println(What is your name?);
+String name =  input.nextLine();
